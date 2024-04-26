9d0bd288f0489a4439cf554e9bd121b2db9d69f0
Dennis Welu <dennis.welu@vueworks.com>
2024-04-26 10:40:35 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EditableReadOnlyEntry
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EditableReadOnlyEntry
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	}
+
+}
